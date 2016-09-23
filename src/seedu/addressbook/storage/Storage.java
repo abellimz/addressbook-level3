fd2fca5e560c086da5ffec932852f6554adaca7c
abellimz <abellim1309@gmail.com>
2016-09-23 17:28:15 +0800

    Add Storage abstract class

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,17 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+/**
+ * Abstract class with methods required for Logic
+ * @author Abel
+ *
+ */
+public abstract class Storage {
+    
+    public abstract void save(AddressBook addressBook) throws StorageOperationException;
+    public abstract AddressBook load() throws StorageOperationException;
+    public abstract String getPath();
+    
+}
